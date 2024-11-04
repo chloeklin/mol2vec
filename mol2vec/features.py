73f70542061c6423d790e7b4e51fab65c9795a69
Han-Ni Lin <u6788552@anu.edu.au>
2024-11-04 11:19:28 +1100

    fix features.py for latest version of gensim

--- mol2vec/features.py
+++ mol2vec/features.py
@@ -422,7 +422,7 @@ def sentences2vec(sentences, model, unseen=None):
     -------
     np.array
     """
-    keys = set(model.wv.vocab.keys())
+    keys = set(model.wv.index_to_key) #OLD: model.wv.vocab.keys()
     vec = []
     if unseen:
         unseen_vec = model.wv.word_vec(unseen)
